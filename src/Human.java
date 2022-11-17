1d437cde2eb29072d2c440d83a1be6a3e28a180b
Igor <99yearago99@gmail.com>
2022-11-17 18:23:22 +0300

    Task 1 and 2.

--- src/Human.java
+++ src/Human.java
@@ -2,5 +2,36 @@ import javax.xml.namespace.QName;
 
 public class Human {
 
+    int yearOfBirth;
+    String name;
+    String town;
+    String jobTitle;
 
+    Human(int yearOfBirth, String name, String town, String jobTitle) {
+
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getTown() {
+        return this.town;
+    }
+
+    public String getJobTitle() {
+        return this.jobTitle;
+    }
+
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        Human Maksim = new Human(1988, "Максим", "Минск","бренд-менеджер");
+            System.out.println(Maksim);
+        Human Anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+            System.out.println(Anya);
+        Human Katya = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
+            System.out.println(Katya);
+        Human Artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
+            System.out.println(Artem);
 
     }
 }
